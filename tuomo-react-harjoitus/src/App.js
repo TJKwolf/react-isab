da7f2f96caaded997889d93f643335cda1768372
Your Name <you@example.com>
2023-02-09 19:33:24 +0200

    nappi

---
+++ tuomo-react-harjoitus/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Message } from "./pages/message";
+import { Buttons } from "./pages/buttons";
+
+function App() {
+  return (
+    <div >
+    <Message />
+    <Buttons />
+    </div>
+  );
+}
+
+export default App;
---
+++ tuomo-react-harjoitus/src/pages/message.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Message = () =>{
+    return(
+        <div>
+            <h1>Hello Isad</h1>
+        </div>
+    );
+}

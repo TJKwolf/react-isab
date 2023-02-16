b6cde604de9f890defcbfe4e79163a147ad77d2e
Your Name <you@example.com>
2023-02-16 19:28:34 +0200

    napit valmiik ja router

--- tuomo-react-harjoitus/src/pages/buttons.jsx
+++ tuomo-react-harjoitus/src/pages/buttons.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import {useState} from "react";
 
 export const Buttons = () => {
+    const[count, setCount] = useState(0);
+    const[count2, setCoun2t] = useState(0);
+    function countClicks(){
+        setCount(count +1);
+    }
+    function countClicks2(){
+        setCoun2t(count2 +1);
+    }
+    function clearCount(){
+        setCount(0);
+        setCoun2t(0);
+    }
     return(
         <div>
-            <button>Sauda</button>
+            <button onClick={countClicks}>sauda: {count} times!</button>
+            <br></br>
+            <button onClick={clearCount}>reset:</button>
+            <button onClick={countClicks2}>sauda: {count2} times!</button>
         </div>
     );
 }

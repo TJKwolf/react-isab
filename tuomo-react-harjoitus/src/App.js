31c57bb330ab522b782b16d7fd6d2173cad3927f
Your Name <you@example.com>
2023-02-23 19:32:19 +0200

    router valmiiksi

--- tuomo-react-harjoitus/src/App.js
+++ tuomo-react-harjoitus/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Message } from "./pages/message";
 import { Buttons } from "./pages/buttons";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Router } from "./pages/router";
 
 function App() {
   return (
-    <div >
-    <Message />
-    <Buttons />
-    </div>
+    <BrowserRouter>
+      <div >
+          <Routes> 
+          <Route path='/'element={<Router/>}>
+          <Route index element={<Message/>}/>
+          <Route path='Buttons' element={<Buttons/>}/>
+        </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 

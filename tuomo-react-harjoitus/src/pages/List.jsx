88a4664f8ea71794540f9587e50b03ee3464fd34
Your Name <you@example.com>
2023-03-30 19:30:28 +0300

    lista tehty

--- tuomo-react-harjoitus/src/pages/List.jsx
+++ tuomo-react-harjoitus/src/pages/List.jsx
@@ -1,16 +1,39 @@
+import { clear } from "@testing-library/user-event/dist/clear";
 import React from "react";
 import { useState } from "react";
+
 
 export const List=()=>{
     const[content, setContent] = useState ([]);
     const [ inputValue, setInputValue ] = useState('');
+    function clearList(){
+        setContent([]);
+    }
     return(
         <div>
              <div>
-                <form>
-                    <input type="Text" placeholder="type here..." required/>
+                <form onSubmit={(event)=>{
+                    event.preventDefault()
+                    const temp =[inputValue,...content];
+                    setContent(temp);
+                    setInputValue('');
+                }}>
+                    <input type="Text" placeholder="type here..." value={inputValue}onChange={e=>setInputValue(e.target.value)} required/>
                     <input type="submit" value="Samo"/>
+                    {
+                        content.map((item)=>(
+                            <ul>
+                                <li>
+                                    <h2> {item} </h2>
+                                </li>
+                            </ul>
+                        ))
+                    }
                 </form>
+                
+                    
+                 
+                <button onClick={clearList}>clear</button>
             </div>
         </div>
     );

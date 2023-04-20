8264f0fa4eee4285f6565cf7e5d1ca9c9cbaff45
Your Name <you@example.com>
2023-04-20 19:31:11 +0300

    on square kesken

--- tuomo-react-harjoitus/src/TicTacToe/TicTacToe.jsx
+++ tuomo-react-harjoitus/src/TicTacToe/TicTacToe.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import './TicTacToe.css';
 import { Board } from "./board";
+import {useState} from "react";
+import { useActionData } from "react-router-dom";
+
+const intialBoard = ['','','','','','','','',''];
+
 export const TicTacToe=()=>{
+    const[gameState, setGameState] = useState(intialBoard);
+    const[isXTurn, setIsXTurn] = useState(true);
+
+    const onSquareClick = () =>{
+        let strings = Array.form(gameState);
+    }
     return(
         <div>
             <h1>Tic-Tac-Toe</h1>
-            <Board/>
+            <Board gameState={gameState}/>
         </div>
     )
 }
